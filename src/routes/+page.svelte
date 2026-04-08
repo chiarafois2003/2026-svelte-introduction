883a479871456c4d4d519e971a8609a0f18fabc4
chiarafois2003 <foischiara20@gmail.com>
2026-04-08 17:41:13 +0200

    add conditional rendering for count display in ClicksCounter component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,9 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <ClicksCounter bind:initialCount={count} />
-<p>Count from parent is {count}</p>
+  {#if count > 10 && count < 20}
+    <p>Count from parent is {count}</p>
+  {/if}
 <ClicksCounter initialCount={3} />
 
 <CanvasSquare />
